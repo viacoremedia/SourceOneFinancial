a0c35b658dad344968ea6f0e8179bdc87f0adc57
JoshGoold <joshgoold2@hotmail.com>
2026-03-25 11:35:00 -0400

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const webhookRoutes = require('./webhook/routes');
 
 const app = express();
